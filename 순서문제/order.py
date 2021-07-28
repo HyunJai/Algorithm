3d159a829da948a1d9c93982cf31da32b68b2922
Hyun Jae Bae <jason0425@g.skku.edu>
2021-07-28 22:13:07 +0900

    Update order.py

--- 순서문제/order.py
+++ 순서문제/order.py
@@ -14,9 +14,9 @@ l = {}
 for i in range(n):
     k= input()
     if k in l:
-        l[k]+=1
+        l[k]+=1 # 2. bcdef가 딕셔너리 안에 있으므로 2로 만들기
     else:
-        l[k] = 1
+        l[k] = 1    # 1. bcdef가 없었으므로 1로 만들기
 print(len(l))
 for i in l:
-    print(l[i], end=" ")
+    print(l[i], end=" ")    # 2 1 1
